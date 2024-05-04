0634b1a3877e4468cbadacb571c02bb15b0178a4
Umut Y. Çalışkan <umutyasin2001@gmail.com>
2024-05-04 23:01:00 +0300

    Refactor LoginNavigationBar component and update CSS styles

--- src/components/LoginNavigationbar.jsx
+++ src/components/LoginNavigationbar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import "./NavigationBar.css"; // Import CSS file for styling
+import { ReactComponent as PawIcon } from "../assets/paw-solid-white.svg"; // Import SVG as a React component
+
+import "./NavigationBar.css";
 
 const LoginNavigationBar = () => {
   return (
@@ -7,23 +9,14 @@ const LoginNavigationBar = () => {
       <nav className="navbar-size">
         <ul className="nav-list">
           <li className="nav-item">
-            <a href="#" className="nav-link">
-              Home
+            <a href="/">
+              {/* Use imported SVG component and set width and height */}
+              <PawIcon alt="Paw Icon" className="nav-icon" />
             </a>
           </li>
           <li className="nav-item">
-            <a href="#" className="nav-link">
-              About
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="#" className="nav-link">
-              Services
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="#" className="nav-link">
-              Contact
+            <a href="/help" className="nav-link">
+              Help
             </a>
           </li>
         </ul>
--- src/pages/help.jsx
+++ src/pages/help.jsx
@@ -1,6 +1,11 @@
+import React from "react";
+
+import LoginNavigationBar from "../components/LoginNavigationbar";
+
 export default function Help() {
   return (
     <div>
+      <LoginNavigationBar></LoginNavigationBar>
       <h1>Help Page</h1>
     </div>
   );
