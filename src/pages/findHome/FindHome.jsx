c18d85d1d69d23d94e015584009e42e98c16cdc8
Aspendas <umutyasin2001@gmail.com>
2024-05-18 21:13:50 +0300

    dummy

--- src/pages/findHome/FindHome.jsx
+++ src/pages/findHome/FindHome.jsx
@@ -2,13 +2,29 @@ import React from "react";
 import HomeNavigationBar from "../../components/HomeNavigationbar";
 
 import "./findHome.css";
+import Footer from "../../components/footer/Footer";
 
 const FindHome = () => {
   return (
-    <div>
+    <div className="find-home-container">
       <HomeNavigationBar></HomeNavigationBar>
-      <h1>Find a Home for a Pet</h1>
-      <ul></ul>
+      <div className="find-home-content-container">
+        <h1 className="find-home-header">Find Home For Your Pet</h1>
+        <p className="find-home-text"> Lorem ipsum dolor amet</p>
+        <div className="create-pet-listing">
+          <div className="create-pet-listing-input-container">
+            <label className="create-pet-listing-label-text">Header</label>
+            <input
+              className="create-pet-listing-text-input-field"
+              dir="ltr"
+              id="title"
+              placeholder="Enter a header for pet listing e.g: Playful Bulldog"
+              type="text"
+            />
+          </div>
+        </div>
+      </div>
+      <Footer></Footer>
     </div>
   );
 };
