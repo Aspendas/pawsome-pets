890d75b96b76326800c9506aae25c80c4e33a333
Umut Y. Çalışkan <umutyasin2001@gmail.com>
2024-05-26 20:59:06 +0300

    help page update

--- src/pages/help/Help.jsx
+++ src/pages/help/Help.jsx
@@ -9,30 +9,47 @@ export default function Help() {
       <LoginNavigationBar></LoginNavigationBar>
       <div className="help-container">
         <h1 className="help-header">Getting Started</h1>
-        <p>
+        <p className="help-paragraph">
           To get started, you will need to create an account. You can do this by
           clicking on the "Sign Up" link at the top of the homepage. Once you
           have created an account, you can log in by entering your email address
           and password on the homepage.
         </p>
-        <h2 classname="help-header">Finding a home for your pet's clubs</h2>{" "}
-        <p classname="help-paragraph">
+        <h2 className="help-subheader">Finding a home for your pet's clubs</h2>{" "}
+        <p className="help-paragraph help-subparagraph">
           If you are looking to find a home for your pet's cubs, you can create
           a listing on our web site. To create a listing, follow these steps:
           <br></br>
-          <ol>
-            <li>Click on the "Create a Listing" link on the homepage.</li>
-            <li>Select the type of pet you are listing.</li>
-            <li>Enter the breed of your pet.</li>
-            <li>Enter the number of cubs you have available.</li>
+          <ol className="ordered-list">
+            <li>Click on the "Find Home" button on the homepage.</li>
+            <li>Upload a photo of your pet</li>
+            <li>Enter the title of your listing.</li>
+            <li>
+              Select the type, age, gender, behaviour of pet you are listing.
+            </li>
+
+            <li>Enter the contact number for communication.</li>
             <li>
               Include a description of your cubs, including their age,
               temperment, and vaccination status.
             </li>
             <li>
-              Upload photos of your cubs. 7. Once you have completed all of the
-              required fields, click on the "Submit" button.
+              Once you have completed all of the required fields, click on the
+              "Submit" button.
             </li>
+          </ol>
+        </p>
+        <h2 className="help-subheader">Finding pet for your home</h2>{" "}
+        <p className="help-paragraph help-subparagraph">
+          If you are looking for a pet', you can search through listing on our
+          web site. To find pet, follow these steps:
+          <br></br>
+          <ol className="ordered-list">
+            <li>Click on the "Find Pet" button on the homepage.</li>
+            <li>Upload a photo of your pet</li>
+            <li>Navigate through various listings</li>
+            <li>Find suitable pet for yourself.</li>
+            <li>Contact with the pet owner using "Contact Information"</li>
           </ol>
         </p>
       </div>
